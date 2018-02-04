b6ff1bd7a3ec64552958f1c768f650a257adb79c
legndery <ar_itra@yahoo.com>
2018-02-05 04:19:11 +0530

    added style

--- react-version/src/search-component/SearchComponent.js
+++ react-version/src/search-component/SearchComponent.js
@@ -3,6 +3,9 @@ import Modal from 'react-modal'
 import { Link } from 'react-router-dom'
 import { Redirect } from 'react-router'
 
+import './SearchConponent.css'
+import arrow from '../assets/arrow.png'
+
 class SearchComponent extends Component {
 
     constructor(props) {
@@ -14,11 +17,12 @@ class SearchComponent extends Component {
                     backgroundColor: 'rgba(0, 0, 0, 0.75)'
                 },
                 content: {
-                    height: '250px',
+                    height: '200px',
                     width: '400px',
                     top: '50%',
                     left: '50%',
                     transform: 'translate(-50%,-50%)',
+                    padding: '20px 25px'
                 }
             },
             searched: false,
@@ -47,24 +51,24 @@ class SearchComponent extends Component {
             if (value !== '' && value.length > 2) {
                 if (!this.state.limitInvalid) {
                     this.setState({ artistNameInvalid: false, formInvalid: false });
-                }else{
-                    this.setState({ artistNameInvalid: false});
+                } else {
+                    this.setState({ artistNameInvalid: false });
                 }
             }
-            else{
-                this.setState({ artistNameInvalid: true, formInvalid:true });
+            else {
+                this.setState({ artistNameInvalid: true, formInvalid: true });
             }
         } else if (name == 'limit') {
             if (!isNaN(value) && value > 0) {
                 if (!this.state.artistNameInvalid) {
                     this.setState({ limitInvalid: false, formInvalid: false });
-                }else{
-                    this.setState({ limitInvalid: false});
+                } else {
+                    this.setState({ limitInvalid: false });
                 }
-            }else{
+            } else {
                 this.setState({ limitInvalid: true, formInvalid: true });
             }
-            
+
         }
     }
     render() {
@@ -73,21 +77,34 @@ class SearchComponent extends Component {
             return (<Redirect to={url} />)
         }
         return (
-            <div>
-                <p>Find your artist below</p>
-                <button onClick={this.openModal}>Search artist</button>
+            <div className='search-component'>
+                <div className='container'>
+                    <img src={arrow} className='arrow-img' />
+                    <p className='title'>Find your artist below</p>
+                    <button onClick={this.openModal} className='button'>Search artist</button>
 
-                <Modal
-                    isOpen={this.state.modalOpened}
-                    style={this.state.modalStyle}
-                    contentLabel="Modal">
-                    <h1>Enter Search Criteria</h1>
-                    <label htmlFor='artistName'>Artist Name</label>
-                    <input type='text' id='artistName' name='artistName' value={this.state.artistName} onChange={this.handleUserInput} />
-                    <label htmlFor='limit'>Number of Tracks</label>
-                    <input type='number' id='limit' name='limit' value={this.state.limit} onChange={this.handleUserInput} />
-                    <button onClick={this.searchTracks} disabled={this.state.formInvalid}>Search</button>
-                </Modal>
+                    <Modal
+                        isOpen={this.state.modalOpened}
+                        style={this.state.modalStyle}
+                        contentLabel="Modal">
+                        <span className='crossBtn' onClick={e => this.setState({modalOpened:false})}>x</span>
+                        <p className='title'>Enter Search Criteria</p>
+                        <table><tbody>
+                            <tr>
+                                <td><label htmlFor='artistName'>Artist Name</label></td>
+                                <td>
+                                    <input type='text' id='artistName' name='artistName' value={this.state.artistName} onChange={this.handleUserInput} />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td><label htmlFor='limit'>No. of Tracks</label></td>
+                                <td><input type='number' id='limit' name='limit' value={this.state.limit} onChange={this.handleUserInput} /></td>
+                            </tr>
+                            <tr><td></td><td><button onClick={this.searchTracks} disabled={this.state.formInvalid} className='button'>Search</button>
+                    </td></tr>
+                        </tbody></table>
+                        </Modal>
+                </div>
             </div>
         );
     }
